7c14d908f7604e62c4102628e5dfae7031ad832e
Sedi98 <sedi9816@gmail.com>
2024-04-14 14:10:44 +0400

    initial

---
+++ index.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const app=express();
+const port= 3000;
+const blog = { id: 1, title: "Blog title", description: "Blog description" }
+
+app.get('/',(req,res)=>{
+    res.send(blog)
+
+})
+
+app.listen(port, ()=>{
+    console.log(`server started at ${port}`)
+})
