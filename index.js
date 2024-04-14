309cb56898a98d9b56e0da08ed1fa0bf34a7dc3e
Sedi98 <sedi9816@gmail.com>
2024-04-14 18:41:01 +0400

    fixed folders added ejs and partials for better performance

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express= require('express');
+const path =require('path')
 const app=express();
 const port= 3000;
-const blog = { id: 1, title: "Blog title", description: "Blog description" }
+// const blog = { id: 1, title: "Blog title", description: "Blog description" };
+app.use(express.static('public'));
+
+const ejs = require('ejs')
+
+app.set('view engine', 'ejs')
 
 app.get('/',(req,res)=>{
-    res.send(blog)
+    res.render('index')
+    
+
+})
+
+
+
+app.get('/about',(req,res)=>{
+    
+    res.render('about')
+
+})
+
+app.get('/add_post',(req,res)=>{
+    
+    res.render('add_post')
+
+})
+
+app.get('/post',(req,res)=>{
+    
+    res.render('post')
 
 })
 
